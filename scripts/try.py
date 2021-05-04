3ce6b03fe7357aaf10331a354d9137feb84b9dad
Cole Arendt <cole.arendt@outlook.com>
2021-05-04 15:55:29 -0400

    add explanatory comment to try.py

--- scripts/try.py
+++ scripts/try.py
@@ -1,6 +1,7 @@
 import rsconnect.api as rscapi
 from dotenv import load_dotenv
 
+# define CONNECT_SERVER and CONNECT_API_KEY env vars in the ".env" file
 load_dotenv()
 
 myrsc_raw = rscapi.rstudio_connect()
